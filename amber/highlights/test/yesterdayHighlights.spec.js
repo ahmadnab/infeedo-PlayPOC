efbf444f20521bff77b804ffd46e82fd81846cdd
ashika099 <kkashikamai@gmail.com>
2024-09-17 20:01:24 +0530

    basic

---
+++ amber/highlights/test/yesterdayHighlights.spec.js
@@ -0,0 +1,20 @@
+const { test, expect, devices } = require('@playwright/test');
+const selector = require('../../login/components/loginComponents.js');
+ const LoginPage = require('../../login/pages/loginPage.js');
+const NavigateToYesterdayHighlights = require('../pages/yesterdayHighlights');
+const loginData = require('../../../test-data/loginData.json');
+const config = require('../../../test-data/config.json');
+
+
+test.describe('Verify Highlights are consistent with respective module', () => {
+
+    test('Verify Highlights are consistent with respective module', async ({ page }) => {
+    
+        const loginPage = new LoginPage(page);
+        await loginPage.login('dashboard');
+        const navigateToYesterdayHighlights = new NavigateToYesterdayHighlights(page);
+        await navigateToYesterdayHighlights.getDetailsYesterdayHighlights();
+    
+     });
+    
+    });
